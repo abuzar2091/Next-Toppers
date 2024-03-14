f714d6383ffbe7138d286102d81fc1897b266c7c
Abuzar <abuzar0561446@gmail.com>
2024-03-14 10:14:00 +0530

    Added individual course cards with clickable functionality for detailed viewing

---
+++ src/shared/FreelanceEduCourses.jsx
@@ -0,0 +1,50 @@
+// import React from "react";
+
+// function FreelanceEduCourses() {
+//   return <div>FreelanceEduCourses</div>;
+// }
+
+// export default FreelanceEduCourses;
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Link } from "react-router-dom";
+// import { Button, Text, Heading, Img } from "./..";
+
+export default function FreelanceEduCourses({
+  standard = "Standard One",
+  standardisa = "Standard 1 is a foundation Standard that reflects 7 important concepts...",
+  classDetails = "Class Details",
+  img="/assets/icons/img_group.svg",
+  ...props
+}) {
+  return (
+    <div {...props}>
+      <div className="flex flex-col items-center  justify-center w-[100%] gap-[25px] p-[15px] bg-white rounded-[15px] hover:shadow-xs ">
+        <div className="flex flex-col items-center justify-start w-full  mt-[15px] gap-[19px] md:px-5 max-w-[260px]">
+          <img
+            src={img}
+            alt="image"
+            className="h-[50px] w-[50px]"
+          />
+          <div className="flex flex-col items-center justify-start w-full gap-[9px]">
+            <h1 className="text-center text-[25px] font-semibold min-w-[260px]">
+              {standard}
+            </h1>
+            <p className="!text-gray-700 text-center !leading-[30px]">
+              {standardisa}
+            </p>
+          </div>
+        </div>
+        <Link to="/course-details">
+        <button
+         
+          className="mb-[15px] sm:px-5 font-medium min-w-[159px]  rounded-[5px]  h-[43px] px-[30px] text-base border-red-400 border border-solid text-red-400"
+        >
+          {classDetails}
+        </button>
+
+        </Link>
+      </div>
+    </div>
+  );
+}
