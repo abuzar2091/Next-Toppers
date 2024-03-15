b6c46bc964d1c289478db9a45ce066c884d62f5a
Abuzar <abuzar0561446@gmail.com>
2024-03-16 05:22:43 +0530

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useSignOutAccount } from "@/lib/react-query/queryAndMutation";
-import { usePopover } from "@/context/PopoverContext";
+
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { SignUpForm } from "@/auth/forms/Index";
 import SignInForm from "@/auth/forms/SignInForm";
@@ -45,51 +45,67 @@ function Header() {
 
   let navItems = [
     { name: "Home", redirection: "/", active: true },
+    { name: "Courses", redirection: "/courses", active: true },
+    { name: "Shop", redirection: "/shop", active: true },
+    { name: "Mentors", redirection: "/mentors", active: true },
+    { name: "Join As a Mentor", redirection: "/mentor", active: true },
     { name: "Login", redirection: "/sign-in", active: !isAuthenticated },
     { name: "Signup", redirection: "/sign-up", active: !isAuthenticated },
-    { name: "All Posts", redirection: "/all-posts", active: isAuthenticated },
-    { name: "Add Post", redirection: "/add-post", active: isAuthenticated },
   ];
   return (
-    <div
-      className={`h-[580px] w-full relative  ${
-        ["sign-in", "sign-up"].find((items) => pathname.includes(items))
-          ? " bg-[rgba(138,133,133,0.5)] -z-6"
-          : ""
-      }`}
-    >
-      <ul className=" flex ml-auto">
-        {navItems.map((item) =>
-          item.active ? (
-            <li key={item.name}>
-              <NavLink
-                to={`${item.redirection}`}
-                // onClick={item.redirection === "/sign-up" ? togglePopover : null}
-                className={({ isActive }) =>
-                  `inline-block px-6 py-2 duration-200 hover:bg-blue-100
+    <div className="">
+      <div
+        className={` w-full relative bg-gray-100 flex px-12 py-3 ${
+          ["sign-in", "sign-up"].find((items) => pathname.includes(items))
+            ? "h-screen bg-[rgba(138,133,133,0.5)] -z-6"
+            : ""
+        }`}
+      >
+        <img
+          loading="lazy"
+          src="/assets/images/freelanceItlogo.png"
+          className="h-[50px]"
+        />
+        <ul className=" flex ml-auto">
+          {navItems.map((item) =>
+            item.active ? (
+              <li key={item.name}>
+                <NavLink
+                  to={`${item.redirection}`}
+                  className={({ isActive }) =>
+                    ` inline-block px-6 py-2 duration-200 hover:bg-blue-100
                                         rounded-full
                                          ${
                                            isActive
                                              ? "text-blue-700 "
                                              : "text-white-700"
                                          }`
-                }
-              >
-                {item.name}
-              </NavLink>
-            </li>
-          ) : null
-        )}
-        {isAuthenticated && (
-          <Button
-            variant="ghost"
-            className="shad-button_ghost mt-4"
-            onClick={handleSignOut}
-          >
-            <p className="small-medium lg:base-medium">Logout</p>
-          </Button>
-        )}
-      </ul>
+                  }
+                >
+                  {item.name}
+                </NavLink>
+              </li>
+            ) : null
+          )}
+          {isAuthenticated && (
+            <div className="flex items-start gap-4">
+              <button onClick={handleSignOut} className="px-2 py-[6px]">
+                Sign out
+              </button>
+              <div className="flex justify-center items-start  gap-2">
+                <button>
+                  <p className="small-medium lg:base-medium py-1">My Account</p>
+                </button>
+                <img
+                  src="/assets/icons/img_profile_24_outline.svg"
+                  alt="profiletwentyfo"
+                  className="h-[30px] w-[30px] mt-1"
+                />
+              </div>
+            </div>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
