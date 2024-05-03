efcc7296daf81b06d46331972e8fd678e2845fa8
Abuzar <abuzar0561446@gmail.com>
2024-05-03 22:02:02 +0530

    setup the standard api response and error format

---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app =express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({ limit: "16kb" })); 
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // for url  encoded data
+app.use(express.static("public"));  // to serve static files css js
+app.use(cookieParser());
+
+export {app}
