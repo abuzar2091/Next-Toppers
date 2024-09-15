bcd852a468c67ba0b11a61958a563e58fc427d42
Abuzar <abuzar0561446@gmail.com>
2024-09-15 13:14:21 +0530

    final deployment

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import QueryProvider from './lib/react-query/QueryProvider.jsx'
 import AuthProvider from './context/AuthContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
-    <QueryProvider>
     <AuthProvider>
     <App />
     </AuthProvider>
-    </QueryProvider>
+  
   </BrowserRouter>
   </React.StrictMode>
 )
